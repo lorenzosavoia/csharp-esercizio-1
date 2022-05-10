4b645acdfc39f223b51e3a6c46f0d45404d5cd73
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-05-10 17:26:41 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Inserisci il primo numero:");
+
+            int iPrimoNumero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserisci il secondo mumero:");
+
+            int iSecondoNumero = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("La moltiplicazione é: {0}",multiplication(iPrimoNumero, iSecondoNumero));
+
+        }
+
+        static int multiplication(int v1 , int v2)
+        {
+            return v1 * v2;
+        }
+    }
+}
